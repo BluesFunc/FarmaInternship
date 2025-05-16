c71b7d2e80b3a4e7fd7312f12e725982bc385e0a
Alex <aleksejmakarov432@gmail.com>
2025-05-16 11:09:11 +0300

    Create entity abstractions

---
+++ Backend/CoreService/Core.Domain/Entities/Abstractions/AuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Domain.Entities.Abstractions;
+
+public abstract class AuditableEntity: Entity
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+    public Guid CreatorId { get; protected set; }
+    public Guid ModifierId { get; protected set; }
+}
---
+++ Backend/CoreService/Core.Domain/Entities/Abstractions/Entity.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Domain.Entities.Abstractions;
+
+public abstract class Entity
+{
+    [Key]
+    public virtual  Guid Id {get; protected set;}
+}
