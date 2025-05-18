7fdcbbd2517fbd4622b05e343875a549ed81f0d6
Alex <aleksejmakarov432@gmail.com>
2025-05-18 20:06:58 +0300

    Add repositories

---
+++ Backend/CoreService/Core.Domain/Interfaces/Repositories/IPaginatedRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Domain.Entities.Abstractions;
+
+namespace Core.Domain.Interfaces.Repositories;
+
+public interface IPaginatedRepository<T>: IRepository<T>
+    where T : Entity
+{
+    public Task<IReadOnlyCollection<Entity>> GetPaginatedAsync(int pageNo = 1 , int pageSize = 10);
+}
---
+++ Backend/CoreService/Core.Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Domain.Entities.Abstractions;
+
+namespace Core.Domain.Interfaces.Repositories;
+
+public interface IRepository<T> where T : Entity
+{
+    public Task<Entity> AddAsync(T entity);
+    public Task<Entity> GetByIdAsync(Guid id);
+    public Entity Update(T entity);
+    public void Delete(Guid id);
+}
