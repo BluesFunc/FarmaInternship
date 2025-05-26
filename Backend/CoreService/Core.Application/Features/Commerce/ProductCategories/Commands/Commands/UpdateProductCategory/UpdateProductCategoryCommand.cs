0367790d098ab8e2e242a6e4a6e28f919994d669
Alex <aleksejmakarov432@gmail.com>
2025-05-26 22:13:41 +0300

    Create ProductCategory features

---
+++ Backend/CoreService/Core.Application/Features/Commerce/ProductCategories/Commands/Commands/UpdateProductCategory/UpdateProductCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using Core.Application.DTOs.Commerce;
+using Core.Application.Wrappers;
+using MediatR;
+
+namespace Core.Application.Features.Commerce.ProductCategories.Commands.Commands.UpdateProductCategory;
+
+public record UpdateProductCategoryCommand : IRequest<Result<ProductCategoryDto>>
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = null!;
+}
---
+++ Backend/CoreService/Core.Application/Features/Commerce/ProductCategories/Commands/Commands/UpdateProductCategory/UpdateProductCategoryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Core.Application.DTOs.Commerce;
+using Core.Application.Features.Abstractions;
+using Core.Application.Wrappers;
+using Core.Application.Wrappers.Enums;
+using Core.Domain.Entities.Abstractions;
+using Core.Domain.Entities.Commerce;
+using Core.Domain.Interfaces.Repositories.Commerce;
+using MapsterMapper;
+using MediatR;
+
+namespace Core.Application.Features.Commerce.ProductCategories.Commands.Commands.UpdateProductCategory;
+
+public class UpdateProductCategoryHandler :
+    SingleRepositoryHandlerBase<IProductCategoryRepository, ProductCategory>,
+    IRequestHandler<UpdateProductCategoryCommand, Result<ProductCategoryDto>>
+{
+    public UpdateProductCategoryHandler(IMapper mapper, IProductCategoryRepository repository) : base(mapper,
+        repository)
+    {
+    }
+
+    public async Task<Result<ProductCategoryDto>> Handle(UpdateProductCategoryCommand request,
+        CancellationToken cancellationToken)
+    {
+        var entity = await _repository.GetByIdAsync(request.Id, cancellationToken);
+        if (entity == null) return Result<ProductCategoryDto>.Failed(ErrorTypeCode.NotFound);
+
+        entity.Name = request.Name;
+        var data = _mapper.Map<ProductCategoryDto>(entity);
+        return Result<ProductCategoryDto>.Successful(data);
+    }
+}
