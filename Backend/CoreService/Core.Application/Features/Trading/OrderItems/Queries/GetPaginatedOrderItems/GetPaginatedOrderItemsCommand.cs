b6f5bdc38871576a9111d06567dc393c5e190715
Alex <aleksejmakarov432@gmail.com>
2025-06-16 10:21:05 +0300

    Create trading pagination use-cases

---
+++ Backend/CoreService/Core.Application/Features/Trading/OrderItems/Queries/GetPaginatedOrderItems/GetPaginatedOrderItemsCommand.cs
@@ -0,0 +1,6 @@
+﻿using Core.Application.Dtos.Trading;
+using Core.Application.Features.Abstractions.Queries.GetPaginatedEntities;
+
+namespace Core.Application.Features.Trading.OrderItems.Queries.GetPaginatedOrderItems;
+
+public record GetPaginatedOrderItemsCommand : GetPaginatedEntitiesCommand<OrderItemDto>;
---
+++ Backend/CoreService/Core.Application/Features/Trading/OrderItems/Queries/GetPaginatedOrderItems/GetPaginatedOrderItemsHandler.cs
@@ -0,0 +1,17 @@
+﻿using Core.Application.Dtos.Trading;
+using Core.Application.Features.Abstractions.Queries.GetPaginatedEntities;
+using Core.Domain.Entities.Trading;
+using Core.Domain.Interfaces.Repositories.Trading;
+using Core.Domain.Models.QueryParams.Trading;
+using MapsterMapper;
+
+namespace Core.Application.Features.Trading.OrderItems.Queries.GetPaginatedOrderItems;
+
+public class GetPaginatedOrderItemsHandler :
+    GetPaginatedEntitiesHandlerBase<IOrderItemRepository, OrderItem, GetPaginatedOrderItemsCommand, OrderItemDto,
+        OrderItemQueryParams>
+{
+    public GetPaginatedOrderItemsHandler(IMapper mapper, IOrderItemRepository repository) : base(mapper, repository)
+    {
+    }
+}
