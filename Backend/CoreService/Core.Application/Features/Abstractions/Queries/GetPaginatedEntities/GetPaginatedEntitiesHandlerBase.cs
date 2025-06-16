d7de6312b0b2334f2b99bc377bd85f28813134eb
Alex <aleksejmakarov432@gmail.com>
2025-06-16 10:19:58 +0300

    Create pagination list and base handler

---
+++ Backend/CoreService/Core.Application/Features/Abstractions/Queries/GetPaginatedEntities/GetPaginatedEntitiesHandlerBase.cs
@@ -0,0 +1,44 @@
+using Core.Application.Wrappers;
+using Core.Application.Wrappers.Enums;
+using Core.Domain.Entities.Abstractions;
+using Core.Domain.Interfaces.Repositories;
+using Core.Domain.Models.QueryParams;
+using Core.Domain.Models.QueryParams.Abstractions;
+using Mapster;
+using MapsterMapper;
+using MediatR;
+
+namespace Core.Application.Features.Abstractions.Queries.GetPaginatedEntities;
+
+public abstract class GetPaginatedEntitiesHandlerBase<TRepository, TEntity, TRequest, TResponse, TFilter> :
+    SingleRepositoryHandlerBase<TRepository, TEntity>
+    , IRequestHandler<TRequest, Result<PaginationList<TResponse>>>
+    where TRepository : IFilteredRepository<TFilter, TEntity>
+    where TEntity : Entity
+    where TRequest : GetPaginatedEntitiesCommand<TResponse>
+    where TResponse : notnull
+    where TFilter : PaginationQueryParams
+{
+    protected GetPaginatedEntitiesHandlerBase(IMapper mapper, TRepository repository) : base(mapper, repository)
+    {
+    }
+
+
+    public async Task<Result<PaginationList<TResponse>>> Handle(TRequest request, CancellationToken cancellationToken)
+    {
+        var filter = request.Adapt<TFilter>();
+
+        var entities = await _repository.GetPaginatedAsync(filter, cancellationToken);
+
+        if (entities is null)
+        {
+            return Result<PaginationList<TResponse>>.Failed(ErrorTypeCode.EntityConflict);
+        }
+
+        var data = _mapper.Map<IReadOnlyList<TResponse>>(entities);
+        var paginationList = new PaginationList<TResponse>(data, request.PageNo, request.PageSize);
+
+
+        return Result<PaginationList<TResponse>>.Successful(paginationList);
+    }
+}
