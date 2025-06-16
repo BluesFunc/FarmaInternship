36c93d2e36de65d87c1091e779ef96bb43d1c21d
Alex <aleksejmakarov432@gmail.com>
2025-06-16 10:20:40 +0300

    Create commerce pagination use-cases

---
+++ Backend/CoreService/Core.Application/Features/Commerce/ProductCategories/Queries/GetPaginatedProductCategories/GetPaginatedProductCategoryCommand.cs
@@ -0,0 +1,9 @@
+﻿using Core.Application.Dtos.Commerce;
+using Core.Application.Features.Abstractions.Queries.GetPaginatedEntities;
+
+namespace Core.Application.Features.Commerce.ProductCategories.Queries.GetPaginatedProductCategories;
+
+public record GetPaginatedProductCategoryCommand : GetPaginatedEntitiesCommand<ProductCategoryDto>
+{
+    public string? Name { get; set; }
+}
---
+++ Backend/CoreService/Core.Application/Features/Commerce/Products/Queries/GetPaginatedProducts/GetPaginatedProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿using Core.Application.Dtos.Commerce;
+using Core.Application.Features.Abstractions.Queries.GetPaginatedEntities;
+using Core.Domain.Entities.Commerce;
+using Core.Domain.Interfaces.Repositories.Commerce;
+using Core.Domain.Models.QueryParams.Commerce;
+using MapsterMapper;
+
+namespace Core.Application.Features.Commerce.Products.Queries.GetPaginatedProducts;
+
+public class GetPaginatedProductsHandler
+    : GetPaginatedEntitiesHandlerBase<IProductRepository, Product, GetPaginatedProductsCommand, ProductDto,
+        ProductQueryParams>
+{
+    public GetPaginatedProductsHandler(IMapper mapper, IProductRepository repository) : base(mapper, repository)
+    {
+    }
+}
