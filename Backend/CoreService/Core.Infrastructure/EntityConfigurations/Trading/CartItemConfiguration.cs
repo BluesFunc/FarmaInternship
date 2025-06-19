71806e4e1be4ce9e0791202b936acb2683a11e90
Alex <aleksejmakarov432@gmail.com>
2025-06-19 13:43:10 +0300

    Create trading entity configurations

---
+++ Backend/CoreService/Core.Infrastructure/EntityConfigurations/Trading/CartItemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.Domain.Entities.Commerce;
+using Core.Domain.Entities.Trading;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Core.Infrastructure.EntityConfigurations.Trading;
+
+public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+{
+    public void Configure(EntityTypeBuilder<CartItem> builder)
+    {
+        builder.ToTable(EntityTablesName.CartItem);
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne<Product>(x => x.ProductObject)
+            .WithMany()
+            .HasForeignKey(x => x.ProductId);
+
+        builder.HasOne<Cart>(cartItem => cartItem.CartObject)
+            .WithMany(cart => cart.CartItems)
+            .HasForeignKey(cartItem => cartItem.CartId);
+    }
+}
---
+++ Backend/CoreService/Core.Infrastructure/EntityConfigurations/Trading/OrderItemConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Core.Domain.Entities.Commerce;
+using Core.Domain.Entities.Trading;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Core.Infrastructure.EntityConfigurations.Trading;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.ToTable(EntityTablesName.OrderItem);
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Quantity)
+            .IsRequired();
+
+        builder.Property(x => x.PriceAtOrder)
+            .IsRequired();
+
+        builder.HasOne<Order>(item => item.OrderObject)
+            .WithMany(order => order.OrderItems)
+            .HasForeignKey(item => item.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<Product>(item => item.ProductObject)
+            .WithMany()
+            .HasForeignKey(item => item.ProductId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
