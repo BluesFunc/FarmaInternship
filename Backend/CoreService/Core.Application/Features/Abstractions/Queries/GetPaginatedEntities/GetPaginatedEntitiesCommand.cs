37192eded9c636ac5d9dad472d84dd0bd8b59047
Alex <aleksejmakarov432@gmail.com>
2025-06-23 09:41:08 +0300

    Aggregate query params into command

--- Backend/CoreService/Core.Application/Features/Abstractions/Queries/GetPaginatedEntities/GetPaginatedEntitiesCommand.cs
+++ Backend/CoreService/Core.Application/Features/Abstractions/Queries/GetPaginatedEntities/GetPaginatedEntitiesCommand.cs
@@ -1,13 +1,17 @@
 ﻿using Core.Application.Interfaces;
 using Core.Application.Wrappers;
+using Core.Domain.Models.QueryParams.Abstractions;
 using MediatR;
 
 namespace Core.Application.Features.Abstractions.Queries.GetPaginatedEntities;
 
-public abstract record GetPaginatedEntitiesCommand<TResponse> : IRequest<Result<PaginationList<TResponse>>>,
-    ITransactionRequest
+public abstract record GetPaginatedEntitiesCommand<TResponse, TFilter>
+    : IRequest<Result<PaginationList<TResponse>>>,
+        ITransactionRequest
     where TResponse : notnull
+    where TFilter : FilterParams
 {
-    public int PageNo { get; init; }
-    public int PageSize { get; init; }
+    public int PageNo { get; init; } = 1;
+    public int PageSize { get; init; } = 5;
+    public TFilter? Filter { get; init; }
 }
--- Backend/CoreService/Core.Application/Features/Commerce/Medicines/Queries/GetPaginatedMedicine/GetPaginatedMedicineCommand.cs
+++ Backend/CoreService/Core.Application/Features/Commerce/Medicines/Queries/GetPaginatedMedicine/GetPaginatedMedicineCommand.cs
@@ -1,8 +1,7 @@
 ﻿using Core.Application.Dtos.Commerce;
 using Core.Application.Features.Abstractions.Queries.GetPaginatedEntities;
+using Core.Domain.Models.QueryParams.Commerce;
 
 namespace Core.Application.Features.Commerce.Medicines.Queries.GetPaginatedMedicine;
 
-public record GetPaginatedMedicineCommand : GetPaginatedEntitiesCommand<MedicineDto>
-{
-}
+public record GetPaginatedMedicineCommand : GetPaginatedEntitiesCommand<MedicineDto, MedicineQueryParams>;
