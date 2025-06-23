602244239e0eeae1a84b363cf0e453d247a6aa03
Alex <aleksejmakarov432@gmail.com>
2025-06-23 12:42:47 +0300

    Create abstract controller. WebApi reference to other project

---
+++ Backend/CoreService/WebApi/Controllers/Abstractions/RestController.cs
@@ -0,0 +1,54 @@
+﻿using Core.Application.Wrappers;
+using Core.Application.Wrappers.Enums;
+using Core.Domain.Entities.Abstractions;
+using Core.Domain.Interfaces.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Abstractions;
+
+[ApiController]
+[Route("[controller]")]
+public abstract class RestController(ISender sender) : ControllerBase
+
+{
+    protected IActionResult ToActionResult(Result result)
+    {
+        
+        return result.ErrorCode switch
+        {
+            ErrorTypeCode.NotFound => NotFound(result.Message),
+            ErrorTypeCode.EntityConflict => BadRequest(result.Message),
+            ErrorTypeCode.ValidationError => UnprocessableEntity(result.Message),
+            ErrorTypeCode.NotAuthorized => Unauthorized(result.Message),
+            ErrorTypeCode.None => Ok(result),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    protected IActionResult ToActionResult<T> (Result<T> result)
+    {
+        return result.ErrorCode switch
+        {
+            ErrorTypeCode.NotFound => NotFound(result.Message),
+            ErrorTypeCode.EntityConflict => BadRequest(result.Message),
+            ErrorTypeCode.ValidationError => UnprocessableEntity(result.Message),
+            ErrorTypeCode.NotAuthorized => Unauthorized(result.Message),
+            ErrorTypeCode.None => Ok(result),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    protected async Task<IActionResult> ExecuteMediatrCommand(IRequest<Result> command)
+    {
+        var result = await sender.Send(command, HttpContext.RequestAborted);
+        return ToActionResult(result);
+    }
+    
+    
+    protected async Task<IActionResult> ExecuteMediatrCommand<T> (IRequest<Result<T>> command)
+    {
+        var result = await sender.Send(command, HttpContext.RequestAborted);
+        return ToActionResult(result);
+    }
+}
