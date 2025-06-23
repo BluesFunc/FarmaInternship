b0131951bef6fb770d7671abdc64e36a1b38d825
Alex <aleksejmakarov432@gmail.com>
2025-06-23 13:58:10 +0300

    Create controllers

---
+++ Backend/CoreService/WebApi/Controllers/Commerce/MerchantController.cs
@@ -0,0 +1,41 @@
+﻿using Core.Application.Dtos.Commerce;
+using Core.Application.Features.Commerce.Medicines.Queries.GetPaginatedMedicine;
+using Core.Application.Features.Commerce.Merchants.Commands.CreateMerchant;
+using Core.Application.Features.Commerce.Merchants.Commands.DeleteMerchantById;
+using Core.Application.Features.Commerce.Merchants.Commands.UpdateMerchant;
+using Core.Application.Features.Commerce.Merchants.Queries.GetMerchantById;
+using Core.Application.Features.Commerce.Merchants.Queries.GetPaginatedMedicine;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers.Abstractions;
+
+namespace WebApi.Controllers.Commerce;
+
+public class MerchantController(ISender sender) : RestController(sender)
+{
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var command = new GetMerchantByIdCommand() { Id = id };
+        return await ExecuteMediatrCommand(command);
+    }
+
+    [HttpPost()]
+    public async Task<IActionResult> Create(CreateMerchantCommand command)
+        => await ExecuteMediatrCommand(command);
+    
+    [HttpDelete()]
+    public async Task<IActionResult> Delete(DeleteMerchantByIdCommand command)
+        => await ExecuteMediatrCommand(command);
+    
+    [HttpPut()]
+    public async Task<IActionResult> Update(UpdateMerchantCommand command)
+        => await ExecuteMediatrCommand(command);
+
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] GetPaginatedMerchantCommand command)
+        => await ExecuteMediatrCommand(command);
+
+
+
+}
---
+++ Backend/CoreService/WebApi/Controllers/Commerce/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Core.Application.Features.Commerce.Medicines.Commands.CreateMedicine;
+using Core.Application.Features.Commerce.Medicines.Commands.DeleteMedicineById;
+using Core.Application.Features.Commerce.Medicines.Commands.UpdateMedicine;
+using Core.Application.Features.Commerce.Medicines.Queries.GetMedicineById;
+using Core.Application.Features.Commerce.ProductCategories.Commands.Commands.DeleteProductCategory;
+using Core.Application.Features.Commerce.ProductCategories.Commands.Commands.UpdateProductCategory;
+using Core.Application.Features.Commerce.Products.Commands.CreateProduct;
+using Core.Application.Features.Commerce.Products.Commands.DeleteProductById;
+using Core.Application.Features.Commerce.Products.Commands.UpdateProduct;
+using Core.Application.Features.Commerce.Products.Queries.GetPaginatedProducts;
+using Core.Application.Features.Commerce.Products.Queries.GetProductById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers.Abstractions;
+
+namespace WebApi.Controllers.Commerce;
+
+public class ProductController(ISender sender) : RestController(sender)
+{
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var command = new GetProductByIdCommand() { Id = id };
+        return await ExecuteMediatrCommand(command);
+    }
+
+    [HttpPost()]
+    public async Task<IActionResult> Create(CreateProductCommand  command)
+        => await ExecuteMediatrCommand(command);
+    
+    [HttpDelete()]
+    public async Task<IActionResult> Delete(DeleteProductByIdCommand command)
+        => await ExecuteMediatrCommand(command);
+    
+    [HttpPut()]
+    public async Task<IActionResult> Update(UpdateProductCommand command)
+        => await ExecuteMediatrCommand(command);
+    
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] GetPaginatedProductsCommand command)
+        => await ExecuteMediatrCommand(command);
+}
