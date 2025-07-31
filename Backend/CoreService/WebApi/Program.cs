9053b914362d12314a6ffecfe87a03efd22c34ce
Alex <aleksejmakarov432@gmail.com>
2025-08-01 00:26:56 +0300

    Integrate Elk

--- Backend/CoreService/WebApi/Program.cs
+++ Backend/CoreService/WebApi/Program.cs
@@ -1,5 +1,8 @@
 using Core.Application;
 using Core.Infrastructure;
+using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.Http;
 
 namespace WebApi;
 
@@ -7,8 +10,21 @@ public class Program
 {
     public static void Main(string[] args)
     {
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.DurableHttpUsingFileSizeRolledBuffers(
+                requestUri: "http://localhost:8080",
+                textFormatter: new JsonFormatter()
+            )
+            .CreateLogger();
+            
         var builder = WebApplication.CreateBuilder(args);
 
+      
+        
         builder.Services.ConfigureApplicationLayer();
         builder.Services.AddInfrastructure();
         builder.Services.AddControllers();
@@ -16,21 +32,24 @@ public class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        builder.Host.UseSerilog();
+
         var app = builder.Build();
 
-
+    
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
+        app.UseSerilogRequestLogging();
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
 
         app.MapControllers();
+        
         app.Run();
     }
 }
